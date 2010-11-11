ba03a5df61297bc5f9b5b4c93966ba3fdc6f7dcd
Pedro Morais <pedro.morais@bitrzr.com>
2010-11-11 20:05:01 +0000

    refactor listing code

--- src/lisbontimemachine/main.py
+++ src/lisbontimemachine/main.py
@@ -19,34 +19,54 @@ def server_call(params):
     else:
         return None
 
+def server_list_photos(latitude, longitude):
+    url = 'collection/fotografias/articles/latitude/%s/longitude/%s' % (latitude, longitude)
+    return server_call(url)
+
+def server_get_photo(article_id):
+    return server_call('collection/fotografias/article/%s' % article_id)
+
+def get_image_elements(latitude, longitude):
+    tree = server_list_photos(latitude, longitude)
+    element = tree.find("listArticles/articles")
+    image_elements = []      
+    for i in element.getchildren():            
+        article_id = i.text        
+        article_element = server_get_photo(article_id)
+        photo_item = {
+            'id': article_id,
+            'title': article_element.findtext('getArticle/article/titulo'),
+            'date': article_element.findtext('getArticle/article/data'),
+            'image_url': article_element.findtext('getArticle/article/image'),
+            'latitude': article_element.findtext('getArticle/article/latitude'),            
+            'longitude': article_element.findtext('getArticle/article/longitude')
+        }
+        
+        image_elements.append(photo_item)
+    return image_elements
 
 
-class ListStuff(webapp.RequestHandler):
+image_element_html_template = """
+<p>
+<h1>%(title)s, %(date)s</h1>
+<h2>%(longitude)s,%(latitude)s</h2>
+<img src="%(image_url)s" alt="%(title)s"/>
+</p>
+"""
+
+class ListHTML(webapp.RequestHandler):
     def get(self):        
         latitude = self.request.get('latitude', '')
         longitude = self.request.get('longitude', '')
-        url = 'collection/fotografias/articles/latitude/%s/longitude/%s' % (latitude, longitude)
-        tree = server_call(url)
-        element = tree.find("listArticles/articles")        
-        for i in element.getchildren():            
-            article_id = i.text
-            
-            article_element = server_call('collection/fotografias/article/%s' % article_id)
-            article_title = article_element.findtext('getArticle/article/titulo')
-            article_date = article_element.findtext('getArticle/article/data')
-            article_image = article_element.findtext('getArticle/article/image')
-            article_latitude = article_element.findtext('getArticle/article/latitude')            
-            article_longitude = article_element.findtext('getArticle/article/longitude')
-            
-            output = """
-<p>
-<h1>%(article_title)s, %(article_date)s</h1>
-<h2>%(article_longitude)s,%(article_latitude)s</h2>
-<img src="%(article_image)s" alt="%(article_title)s"/>
-</p>
-"""  % locals()  
-            
-            self.response.out.write(output)
+        for image_element in get_image_elements(latitude, longitude):
+            self.response.out.write(image_element_html_template % image_element)
+
+class ListJSONL(webapp.RequestHandler):
+    def get(self):        
+        latitude = self.request.get('latitude', '')
+        longitude = self.request.get('longitude', '')
+        for image_element in get_image_elements(latitude, longitude):
+            self.response.out.write(image_element_html_template % image_element)
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
@@ -55,7 +75,7 @@ class MainHandler(webapp.RequestHandler):
 
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler), ('/list', ListStuff)],
+    application = webapp.WSGIApplication([('/', MainHandler), ('/list', ListHTML)],
                                          debug=True)
     util.run_wsgi_app(application)
 
